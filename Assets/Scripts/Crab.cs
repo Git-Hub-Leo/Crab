5eb0560ddc303d2a1ace40aff3e930befde95bfd
Git-Hub-Leo <leo.nossnahoj@gmail.com>
2024-01-09 11:09:40 +0100

    Lobster no longer collides with worm Lobster destroys crab on collision

--- Assets/Scripts/Crab.cs
+++ Assets/Scripts/Crab.cs
@@ -40,10 +40,8 @@ public class Crab : MonoBehaviour
 
         
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-
         if (collision.gameObject.name.StartsWith("worm"))
         {
             Destroy(collision.gameObject);
--- Assets/Scripts/Lobster.cs
+++ Assets/Scripts/Lobster.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class Lobster : MonoBehaviour
 {
     private Rigidbody2D rb;
     private float speed = 3;
+    [SerializeField]
+    private float angularSpeed = 1000;
+
 
 
     // Start is called before the first frame update
@@ -19,6 +23,35 @@ public class Lobster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 1% chans för sväng
+        if (Random.Range(0, 100) < 1)
+        {
+            float angleRadians = rb.rotation * Mathf.PI / 180f;
+            float xSpeed = Mathf.Cos(angleRadians) * speed;
+            float ySpeed = Mathf.Sin(angleRadians) * speed;
+            rb.velocity = new Vector2(xSpeed, ySpeed);
+            // slumpa riktningsförändring
+            float angle = Random.Range(-angularSpeed, angularSpeed) * Time.deltaTime;
+       
+            rb.rotation += angle;
+        }
         
+        
+        // går fram
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+
+        if (collision.gameObject.name.StartsWith("Crab"))
+        {
+            Destroy(collision.gameObject);
+            
+        }
+
+        if (collision.gameObject.name.EndsWith("wall"))
+        { 
+            rb.rotation = Random.Range(120, 180);
+        }
     }
 }
